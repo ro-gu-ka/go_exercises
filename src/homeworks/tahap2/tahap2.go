ef45428c6e338d8cdf34c3825119bdb58a688fa2
ronygustam <66596679+ronygustam@users.noreply.github.com>
2024-02-11 22:57:52 +0700

    Added tugas 2

--- src/homeworks/tahap2/tahap2.go
+++ src/homeworks/tahap2/tahap2.go
@@ -1,12 +1,85 @@
 package tahap2
 
-func GenerateNik(gender string, year int, number_generated int) string {
-	var g string
-	if gender == "male" {
-		g = "N"
-	} else if gender == "female" {
-		g = "T"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"math/rand"
+	"strconv"
+	"strings"
+)
+
+func GenerateConsecutiveNextNik(nik string, numberGenerated int) []string {
+	var coll []string
+
+	splitted := strings.Split(nik, "-")
+	// validation
+	if !strings.Contains(splitted[0], "ARN") || strings.Contains(splitted[0], "ART") {
+		log.Fatal("Invalid NIK format: ", splitted[0])
 	}
-	nik := "AR" + g
+	if len([]rune(splitted[1])) != 5 {
+		log.Fatal("Invalid NIK number format: ", splitted[1])
+	}
+
+	// string to int
+	n, err := strconv.Atoi(splitted[1])
+	if err != nil {
+		// handle error
+		panic(err)
+	}
+	for i := 1; i <= numberGenerated; i++ {
+		n = n + 1
+		coll = append(coll, splitted[0]+"-"+convertToStringNumber(n))
+	}
+
+	return coll
+}
+
+func GenerateNik(gender string, year int) string {
+	// gender code
+	g, err := getGenderCode(gender)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// last 2 digits of year
+	y := strconv.Itoa(year)[2:]
+
+	// nik
+	nik := "AR" + g + y + randomSemester() + "-" + randomNumber()
 	return nik
+}
+
+func GenerateNikCollection(gender string, year int, numberGenerated int) []string {
+	var coll []string
+	for i := 1; i <= numberGenerated; i++ {
+		nik := GenerateNik(gender, year)
+		coll = append(coll, nik)
+	}
+	return coll
+}
+
+func getGenderCode(gender string) (string, error) {
+	if gender == "male" || gender == "m" {
+		return "N", nil
+	} else if gender == "female" || gender == "f" {
+		return "T", nil
+	}
+	return gender, errors.New("Wrong gender input!")
+}
+
+func randomSemester() string {
+	// Only two values, "1" is b/w 1-6 and "2" is b/w 7-12
+	formats := []string{"1", "2"}
+
+	// Return a randomly selected semester
+	return formats[rand.Intn(len(formats))]
+}
+
+func randomNumber() string {
+	return fmt.Sprintf("%05d", rand.Intn(1000))
+}
+
+func convertToStringNumber(num int) string {
+	return fmt.Sprintf("%05d", num)
 }
--- src/homeworks/tugas/tugas.go
+++ src/homeworks/tugas/tugas.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	// "homework.com/tahap1"
 	"homework.com/tahap2"
@@ -11,6 +12,17 @@ func main() {
 	// tahap1.PrintDifference("Hello Sandbox HSI, in ", 2024)
 	// tahap1.TestError()
 
-	nik := tahap2.GenerateNik("female", 2009, 1)
-	fmt.Println(nik)
+	// tugas 2
+	fmt.Println("Random NIK: ")
+	nikColl := tahap2.GenerateNikCollection("m", 2021, 10)
+	fmt.Printf("%q\n", nikColl)
+
+	fmt.Println("Consecutive NIK: ")
+	nikConsecutive := tahap2.GenerateConsecutiveNextNik("ARN212-00122", 10)
+	fmt.Printf("%q\n", nikConsecutive)
+
+	fmt.Println("Sorted NIK: ")
+	coll := []string{"ARN212-00813", "ARN192-00197", "ARN202-00770", "ARN212-00089", "ARN221-00892", "ARN242-00517", "ARN182-00825", "ARN191-00179", "ARN211-00542", "ARN212-00318"}
+	sort.Strings(coll)
+	fmt.Printf("%q\n", coll)
 }
